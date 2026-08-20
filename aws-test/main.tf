410b6f327724b2c36301c06e280fc8262583cc39
Roy Gunter <roygunter@hushmail.com>
2026-08-19 20:41:27 -0400

    Created EKS cluster

--- aws-test/main.tf
+++ aws-test/main.tf
@@ -48,3 +48,122 @@ resource "aws_subnet" "eks_public_b" {
     Name = "roy-eks-public-b"
   }
 }
+
+resource "aws_internet_gateway" "eks" {
+  vpc_id = aws_vpc.eks.id
+
+  tags = {
+    Name = "roy-eks-igw"
+  }
+}
+
+resource "aws_route_table" "eks_public" {
+  vpc_id = aws_vpc.eks.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.eks.id
+  }
+
+  tags = {
+    Name = "roy-eks-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "eks_public_a" {
+  subnet_id      = aws_subnet.eks_public_a.id
+  route_table_id = aws_route_table.eks_public.id
+}
+
+resource "aws_route_table_association" "eks_public_b" {
+  subnet_id      = aws_subnet.eks_public_b.id
+  route_table_id = aws_route_table.eks_public.id
+}
+
+resource "aws_iam_role" "eks_cluster" {
+  name = "roy-eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [{
+      Effect = "Allow"
+
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+
+      Action = "sts:AssumeRole"
+    }]
+  })
+
+  tags = {
+    Name = "roy-eks-cluster-role"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  role       = aws_iam_role.eks_cluster.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_eks_cluster" "eks" {
+  name     = "roy-eks"
+  role_arn = aws_iam_role.eks_cluster.arn
+
+  vpc_config {
+    subnet_ids = [
+      aws_subnet.eks_public_a.id,
+      aws_subnet.eks_public_b.id
+    ]
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_policy
+  ]
+
+  tags = {
+    Name = "roy-eks"
+  }
+}
+
+resource "aws_iam_role" "eks_node" {
+  name = "roy-eks-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [{
+      Effect = "Allow"
+
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+
+      Action = "sts:AssumeRole"
+    }]
+  })
+
+  tags = {
+    Name = "roy-eks-node-role"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
+  role       = aws_iam_role.eks_node.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_ecr_read_only" {
+  role       = aws_iam_role.eks_node.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPullOnly"
+}
+
+
+
+
+
+
+
+
+
