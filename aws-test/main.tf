ae1fe11e1da5da2d2d9441c6a14b9581368e06dd
Roy Gunter <roygunter@hushmail.com>
2026-09-01 21:02:30 -0400

    Move helm and some k8s artifacts to Terraform

--- aws-test/main.tf
+++ aws-test/main.tf
@@ -3,6 +3,19 @@ terraform {
     aws = {
       source = "hashicorp/aws"
     }
+
+    tls = {
+      source = "hashicorp/tls"
+    }
+
+    helm = {
+      source = "hashicorp/helm"
+    }
+
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+
   }
 }
 
@@ -159,11 +172,158 @@ resource "aws_iam_role_policy_attachment" "eks_ecr_read_only" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPullOnly"
 }
 
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_node.name
+}
 
+resource "aws_iam_role_policy_attachment" "eks_node_ssm" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  role       = aws_iam_role.eks_node.name
+}
 
+resource "aws_eks_node_group" "eks_nodes" {
+  cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = "roy-eks-nodes"
+  node_role_arn   = aws_iam_role.eks_node.arn
 
+  subnet_ids = [
+    aws_subnet.eks_public_a.id,
+    aws_subnet.eks_public_b.id
+  ]
 
+  scaling_config {
+    desired_size = 2
+    max_size     = 2
+    min_size     = 2
+  }
 
+  instance_types = ["t3.small"]
 
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_worker_node_policy,
+    aws_iam_role_policy_attachment.eks_ecr_read_only,
+    aws_iam_role_policy_attachment.eks_cni_policy
+  ]
+}
 
+data "tls_certificate" "eks_oidc" {
+  url = aws_eks_cluster.eks.identity[0].oidc[0].issuer
+}
 
+resource "aws_iam_openid_connect_provider" "eks" {
+  url = aws_eks_cluster.eks.identity[0].oidc[0].issuer
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+
+  thumbprint_list = [
+    data.tls_certificate.eks_oidc.certificates[0].sha1_fingerprint
+  ]
+}
+
+resource "aws_iam_role" "aws_load_balancer_controller" {
+  name = "AmazonEKSLoadBalancerControllerRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.eks.arn
+        }
+
+        Action = "sts:AssumeRoleWithWebIdentity"
+
+        Condition = {
+          StringEquals = {
+            "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:aud" = "sts.amazonaws.com"
+            "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub" = "system:serviceaccount:kube-system:aws-load-balancer-controller"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "aws_load_balancer_controller" {
+  name = "AWSLoadBalancerControllerIAMPolicy"
+
+  policy = file("${path.module}/iam_policy.json")
+}
+
+resource "aws_iam_role_policy_attachment" "aws_load_balancer_controller" {
+  role       = aws_iam_role.aws_load_balancer_controller.name
+  policy_arn = aws_iam_policy.aws_load_balancer_controller.arn
+}
+
+data "aws_eks_cluster_auth" "eks" {
+  name = aws_eks_cluster.eks.name
+}
+
+provider "helm" {
+  kubernetes = {
+    host                   = aws_eks_cluster.eks.endpoint
+    cluster_ca_certificate = base64decode(
+      aws_eks_cluster.eks.certificate_authority[0].data
+    )
+    token = data.aws_eks_cluster_auth.eks.token
+  }
+}
+
+resource "helm_release" "aws_load_balancer_controller" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+  version    = "3.5.0"
+
+  set = [
+    {
+      name  = "clusterName"
+      value = aws_eks_cluster.eks.name
+    },
+    {
+      name  = "serviceAccount.create"
+      value = "false"
+    },
+    {
+      name  = "serviceAccount.name"
+      value = "aws-load-balancer-controller"
+    },
+    {
+      name  = "vpcId"
+      value = aws_vpc.eks.id
+    }
+  ]
+
+  depends_on = [
+    kubernetes_service_account_v1.aws_load_balancer_controller
+  ]
+
+}
+
+provider "kubernetes" {
+  host                   = aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(
+    aws_eks_cluster.eks.certificate_authority[0].data
+  )
+  token = data.aws_eks_cluster_auth.eks.token
+}
+
+resource "kubernetes_service_account_v1" "aws_load_balancer_controller" {
+  automount_service_account_token = false
+
+  metadata {
+    name      = "aws-load-balancer-controller"
+    namespace = "kube-system"
+
+    annotations = {
+      "eks.amazonaws.com/role-arn" = aws_iam_role.aws_load_balancer_controller.arn
+    }
+  }
+}
